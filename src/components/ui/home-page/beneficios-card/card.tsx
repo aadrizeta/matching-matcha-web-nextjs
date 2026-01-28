3d78ad04b276b2bb6c30a7068dcce3a0219d4d05
Adrian <adrianzambranomontero@gmail.com>
2026-01-28 13:13:07 +0100

    iconos png concertidos a svg

--- src/components/ui/home-page/beneficios-card/card.tsx
+++ src/components/ui/home-page/beneficios-card/card.tsx
@@ -3,25 +3,25 @@ import "@/app/globals.css";
 
 const Beneficios = [
     {
-        icon: "/icons/Iconos-beneficios/cara.png",
+        icon: "/icons/Iconos-beneficios/cara.svg",
         title: "Mejora la salud de la piel",
         description:
             "Los antioxidantes del té matcha ayudan a combatir el daño causado por los radicales libres, contribuyendo a una piel más sana, luminosa y con signos de envejecimiento reducidos.",
     },
     {
-        icon: "/icons/Iconos-beneficios/salud.png",
+        icon: "/icons/Iconos-beneficios/salud.svg",
         title: "Promueve la salud del corazón",
         description:
             "Consumir matcha puede ayudar a reducir los niveles de colesterol LDL y mejorar la circulación, lo que favorece una mejor salud cardiovascular.",
     },
     {
-        icon: "/icons/Iconos-beneficios/cintura.png",
+        icon: "/icons/Iconos-beneficios/cintura.svg",
         title: "Puede ayudar a la pérdida de peso",
         description:
             "El matcha puede acelerar el metabolismo y aumentar la quema de grasa, especialmente cuando se combina con una alimentación equilibrada y ejercicio regular.",
     },
     {
-        icon: "/icons/Iconos-beneficios/cerebro.png",
+        icon: "/icons/Iconos-beneficios/cerebro.svg",
         title: "Ayuda a mantener la concentración",
         description:
             "Gracias a la L-teanina, el matcha promueve un estado de alerta relajada, mejorando la concentración y la claridad mental sin causar nerviosismo.",
@@ -33,7 +33,7 @@ const Beneficios = [
             "Contiene cafeína de liberación gradual, lo que proporciona energía sostenida y estable a lo largo del día, evitando picos bruscos.",
     },
     {
-        icon: "/icons/Iconos-beneficios/antioxidantes.png",
+        icon: "/icons/Iconos-beneficios/antioxidantes.svg",
         title: "Alta concentración de antioxidantes",
         description:
             "El matcha es especialmente alto en catequinas, potentes antioxidantes que ayudan a proteger las células y fortalecer el sistema inmunológico.",
