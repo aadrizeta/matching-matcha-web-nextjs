c9dfe2d0ae9e9563190e195ae6df5ac68cb922d9
Adrian <adrianzambranomontero@gmail.com>
2026-02-04 12:37:54 +0100

    optimizacion css y pollyfills

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,9 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   output: "standalone",
   reactCompiler: true,
+  experimental: {
+    inlineCss: true,
+  },
 };
 
 export default nextConfig;
